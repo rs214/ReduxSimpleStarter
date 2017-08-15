f45d307839b5f98eac2ad64d169b3f0b1465193a
Robert Si <robertsi0214@gmail.com>
2017-08-14 20:45:53 -0700

    add searchbar to weather app

--- src/components/app.js
+++ src/components/app.js
@@ -1,13 +1,15 @@
 import React, { Component } from 'react';
 import Booklist from '../containers/book_list';
 import BookDetail from '../containers/book-detail';
+import SearchBar from '../containers/searchBar';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Booklist />
-        <BookDetail />
+          <Booklist />
+          <BookDetail />
+          <SearchBar />
       </div>
     );
   }
