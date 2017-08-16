1a47b5b7f57019310ad1035ae61f4bf989d20ab4
Robert Si <robertsi0214@gmail.com>
2017-08-16 16:19:14 -0700

    add searchbar functionality for weather app

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export function selectBook(book) {
   //selectbook is an action creator, an obj with a type property
   //action ALWAYS contains a type, sometimes a payload
@@ -9,3 +11,16 @@ export function selectBook(book) {
 
 
 const API_KEY = 'baeedc82b797ecca66ad5c158d4ec09d';
+const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`
+
+export const FETCH_WEATHER = 'FETCH_WEATHER';
+
+export function fetchWeather(city) {
+  const url = `${ROOT_URL}&q=${city},us`;
+  const request = axios.get(url);
+
+  return {
+    type: FETCH_WEATHER,
+    payload: request
+  };
+}
