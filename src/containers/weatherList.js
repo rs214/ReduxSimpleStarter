ba5b69f78c077ba80ef6f5fd186df355da02b3bc
Robert Si <robertsi0214@gmail.com>
2017-08-18 12:29:02 -0700

    add sparklines to weatherList

--- src/containers/weatherList.js
+++ src/containers/weatherList.js
@@ -1,11 +1,33 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Sparklines, SparklinesLine } from 'react-sparklines';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
+    const name = cityData.city.name;
+    const temps = cityData.list.map(weather => weather.main.temp);
+    const pressure = cityData.list.map(pressure => pressure.main.pressure);
+    const humidity = cityData.list.map(humidity => humidity.main.humidity);
+
+
     return (
-      <tr key={cityData.city.name}>
-        <td>{cityData.city.name}</td>
+      <tr key={name}>
+        <td>{name}</td>
+        <td>
+          <Sparklines height={120} width={180} data={temps}>
+            <SparklinesLine color="blue" />
+          </Sparklines>
+        </td>
+        <td>
+          <Sparklines height={120} width={180} data={pressure}>
+            <SparklinesLine color="blue" />
+          </Sparklines>
+        </td>
+        <td>
+          <Sparklines height={120} width={180} data={humidity}>
+            <SparklinesLine color="blue" />
+          </Sparklines>
+        </td>
       </tr>
     );
   }
